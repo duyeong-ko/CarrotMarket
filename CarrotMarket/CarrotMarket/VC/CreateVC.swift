1bd873222dd05b8ecd5b51b8367c557f78afe6d8
dudu <329_143@naver.com>
2022-12-17 23:36:51 +0900

    [Feat] #3 - 홈뷰 버튼, 화면전환 구현

---
+++ CarrotMarket/CarrotMarket/VC/CreateVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreateVC.swift
+//  CarrotMarket
+//
+//  Created by 고두영 on 2022/12/17.
+//
+
+import UIKit
+
+class CreateVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
